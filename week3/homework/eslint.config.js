aa75d3a9cda307cf5ccd9de2009c222bfd758bb8
jogpfls <joerin4177@gmail.com>
2025-11-05 20:43:42 +0900

    init: prettier, eslint 설정

--- week3/homework/eslint.config.js
+++ week3/homework/eslint.config.js
@@ -1,29 +1,66 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import babelParser from "@babel/eslint-parser";
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ["node_modules", "dist"] },
+
+  js.configs.recommended,
+
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: babelParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
       },
     },
+    settings: {
+      react: { version: "detect" },
+    },
+    plugins: {
+      react,
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...(react.configs?.recommended?.rules ?? {}),
+
+      curly: ["error", "all"],
+      "no-console": "warn",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_|^args$" }],
+
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+
+      "react/jsx-filename-extension": ["error", { extensions: [".jsx"] }],
+
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^node:"],
+            ["^react$", "^@?\\w"],
+            ["^\\u0000"],
+            ["^\\."],
+            ["^.+\\.css$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
     },
   },
-])
+
+  prettierConfig,
+];
