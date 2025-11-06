d8259c9e1d1de11d41a040da335111fcb5ab80ee
jogpfls <joerin4177@gmail.com>
2025-11-07 04:46:04 +0900

    chore: 폴더 구조 수정

--- week3/homework/src/styles/token/color.css.js
+++ week3/homework/src/styles/token/color.css.js
@@ -3,7 +3,8 @@ export const color = {
   blue200: "#E3ECFF",
   blue300: "#CEDDFF",
   blue400: "#A7C8FF",
-  blue500: "#3964AA",
+  blue500: "#4C86E3",
+  blue600: "#25549F",
 
   error100: "#C7837D",
 };
