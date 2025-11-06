20c44349928846cf36be49dad1e9503aa17b9981
jogpfls <joerin4177@gmail.com>
2025-11-07 03:32:09 +0900

    init: 절대 경로 설정

--- week3/homework/eslint.config.js
+++ week3/homework/eslint.config.js
@@ -52,6 +52,7 @@ export default [
           groups: [
             ["^node:"],
             ["^react$", "^@?\\w"],
+            ["^@pages/*", "^@shared/*"],
             ["^\\u0000"],
             ["^\\."],
             ["^.+\\.css$"],
