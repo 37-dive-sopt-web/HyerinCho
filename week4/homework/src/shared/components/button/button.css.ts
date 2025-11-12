43ff12cbe8a6d7cb5ffaa57ee374df698cb1b4ad
jogpfls <joerin4177@gmail.com>
2025-11-12 17:01:35 +0900

    feat: 버튼 컴포넌트 구현

---
+++ week4/homework/src/shared/components/button/button.css.ts
@@ -0,0 +1,20 @@
+import { recipe } from "@vanilla-extract/recipes";
+
+import { color } from "@shared/styles/token/color.css";
+
+export const buttonContainer = recipe({
+  base: {
+    width: "100%",
+    padding: "0.8rem 1rem",
+    borderRadius: "8px",
+    color: color.white,
+    fontSize: "1.6rem",
+    fontWeight: "600",
+  },
+  variants: {
+    disabled: {
+      true: { backgroundColor: color.blue100 },
+      false: { backgroundColor: color.blue200 },
+    },
+  },
+});
