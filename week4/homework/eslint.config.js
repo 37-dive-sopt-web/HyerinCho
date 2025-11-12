092059a268333ea016508b5fb28f4668341431bc
jogpfls <joerin4177@gmail.com>
2025-11-12 15:36:45 +0900

    chore: 절대 경로 수정

--- week4/homework/eslint.config.js
+++ week4/homework/eslint.config.js
@@ -34,10 +34,7 @@ export default [
       "simple-import-sort": simpleImportSort,
     },
     rules: {
-      // 권장 규칙 세트(타입 정보 비필요) + 필요시 type-checked 세트 추가
       ...tseslint.configs.recommended.rules,
-      // 타입정보 기반 체크까지 하고 싶으면 아래도 추가(위 parserOptions.project 필요)
-      // ...(tseslint.configs.recommendedTypeChecked?.rules ?? {}),
 
       ...(react.configs?.recommended?.rules ?? {}),
 
@@ -46,38 +43,31 @@ export default [
       "react/require-default-props": "off",
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
+
       // TSX 허용
       "react/jsx-filename-extension": [
         "error",
         { extensions: [".jsx", ".tsx"] },
       ],
 
-      // Hooks 권장
       ...reactHooks.configs.recommended.rules,
 
-      // 일반 규칙
       curly: ["error", "all"],
       "no-console": "warn",
-      "no-unused-vars": "off", // TS 규칙으로 대체
+      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         { argsIgnorePattern: "^_|^args$" },
       ],
 
-      // import 정렬
       "simple-import-sort/imports": [
         "error",
         {
           groups: [
             ["^node:"],
             ["^react$", "^@?\\w"],
-            [
-              "^@pages(/.*)?$",
-              "^@components(/.*)?$",
-              "^@styles(/.*)?$",
-              "^@utils(/.*)?$",
-            ],
-            ["^\\u0000"], // side-effect imports
+            ["^@pages(/.*)?$", "^@shared(/.*)?$"],
+            ["^\\u0000"],
             ["^\\."],
             ["^.+\\.css$"],
           ],
