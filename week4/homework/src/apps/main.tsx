30b0689a9ec0b1bb7a7a510630ab5872874ec2ae
jogpfls <joerin4177@gmail.com>
2025-11-12 15:37:00 +0900

    init: vanilla-extract 스타일 설정

--- week4/homework/src/apps/main.tsx
+++ week4/homework/src/apps/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "@shared/styles/reset.css.ts";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
---
+++ week4/homework/src/shared/styles/token/color.css.ts
@@ -0,0 +1,11 @@
+export const color = {
+  blue100: "#CADBFF",
+  blue200: "#9EC3FF",
+
+  gray100: "#959595",
+
+  white: "#FFFFFF",
+
+  error: "#FF6557",
+  success: "#",
+};
